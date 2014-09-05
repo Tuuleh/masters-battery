cb68504897885a39e19b505c38bc5257d4290e0a
tuuli <tuuli.pollanen@gmail.com>
2014-09-05 16:42:02 -0700

    the plugin now appends a submit button after the last item in the trial sequence

--- scripts/plugins/clean_dropdown.js
+++ scripts/plugins/clean_dropdown.js
@@ -26,6 +26,7 @@
             for(var i = 0; i < params.items.length; i++) {
                 trials.push({
                     type: 'survey-dropdown',
+                    element: 'item',
                     // other information needed for the trial method can be added here
                     items: params.items[i],
                     options: params.options[i],
@@ -35,42 +36,49 @@
                     data: (typeof params.data === 'undefined') ? {} : params.data[i]
                 });   
             }
+            //push submit button to the end of the trial sequence
+            trials.push({
+                type: "survey-dropdown",
+                element: "submit_button"
+            });
             return trials;
         };
 
         //change this so that you can define several items simultaneously and so it runs for the length of items array
         plugin.trial = function(display_element, block, trial, part) {
-
-            display_element.append("<p>" + trial.items + "</p>");
-            var $main_menu = $("<select class='main_menu' id='main_menu_" + trial.number + "' text='" + trial.items + "'/>");
-
-            for(var val in trial.options) {
-                $("<option />", {value: val, text: trial.options[val]}).appendTo($main_menu);
-
-                if (val == (trial.options.length-1)) {
-                    $main_menu.append("</select>");
-                    display_element.append($main_menu);
-
-                    //constructs submenus if the length of the array containing the submenu options arrays is above 0
-                    if (trial.submenu_options.length != undefined) {
-
-                        for (var i in trial.submenu_options) {  
-
-                            if (trial.submenu_options[i].length > 0) {
-                                var $submenu = $("<select class = 'submenu' id = 'submenu_" + i + "' parent_item='main_menu_" + trial.number +"'/>");
-
-                                for (var j in trial.submenu_options[i]) {
-                                    console.log(trial.submenu_options[i].length);
-                                    var $submenu_option = $("<option />", {value: j, text: trial.submenu_options[i][j]})
-                                    $submenu.append($submenu_option);
+            if (trial.element === "item") {
+                display_element.append("<p>" + trial.items + "</p>");
+                var $main_menu = $("<select class='main_menu' id='main_menu_" + trial.number + "' text='" + trial.items + "'/>");
+                //adds options to the main select
+                for(var val in trial.options) {
+                    $("<option />", {value: val, text: trial.options[val]}).appendTo($main_menu);
+                    //if at the last iteration of the options for the main select, close the select tag and append
+                    //the main select to display element. 
+                    if (val == (trial.options.length-1)) {
+                        display_element.append($main_menu);
+                        if (trial.submenu_options.length != undefined) {   //If submenu option array contains menus...
+                            for (var i in trial.submenu_options) {         //loop through it.
+                                if (trial.submenu_options[i].length > 0) { //And if the submenu array is not empty, create submenu and append it.
+                                    var $submenu = $("<select class = 'submenu' id = 'submenu_" + i + "' parent_item='main_menu_" + trial.number +"'/>");
+                                    for (var j in trial.submenu_options[i]) {
+                                        var $submenu_option = $("<option />", {value: j, text: trial.submenu_options[i][j]})
+                                        $submenu.append($submenu_option);
+                                    }
+                                    display_element.append($submenu);
                                 }
-
-                                $submenu.append("</select");
-                                display_element.append($submenu);
                             }
                         }
-                    }
-                }  
+                    }  
+                }
+            }
+            else if (trial.element === "submit_button") {
+                display_element.append($('<p>'));
+                display_element.append($('<button>', {
+                    'id': 'dropdown-submit',
+                    'class': 'dropdown-submit',
+                    'html': 'Submit Answers',
+                }));
+                $("#dropdown-submit").css('display','block');
             }
 
             //defaults as all submenus hidden
@@ -88,19 +96,35 @@
                 //show if class is submenu, parent item is id of $this, and id is submenu_ + $(this).val()
                 $showable.show();
             });
-
-        
-            block.writeData($.extend({}, {drek: this.value}, trial.data));
-
-                // let's say this is the end here:
             block.next();
 
-var trial_data = {
-type: trial.type,
-trial_index: block.trial_idx,
-main_option: "main_option",
-submenu_option: "submenu_option"
-};
+            //SUBMIT BUTTON-ON-CLICK ITERATE THROUGH DOM AND GET VALS INTO AN OBJECT AND EXTEND IT IN block.writeData.
+        
+            //APPEND A SUBMIT BUTTON HERE
+            
+            /*$("#dropdown-submit").click(function() {
+                // create object to hold responses
+                var dropdown_data = {};
+                //for every main menu...
+                $("div.jspsych-survey-likert-slider").each(function(index) {
+                    var id = "Q" + index;
+                    var mainmenu_val = $(this).val();
+                    var submenu_val = $('select[class="submenu"][parent_item="'+$(this).attr("id")+'"][id="submenu_' + $(this).val() + '"]').val();
+                    //val from submenu where id is "submenu'_+ $(this).val() + "'", or if no such exists, blank
+                    var obje = {};
+                    obje[id] = mainmenu_val;
+                    $.extend(dropdown_data, obje);
+                });
+                // save data
+                block.writeData($.extend({}, {
+                    "trial_type": "dropdown",
+                    "trial_index": block.trial_idx,
+                }, dropdown_data, trial.data));
+
+                display_element.html('');
+                // next trial
+                block.next();
+            }); */
 
         };
 
