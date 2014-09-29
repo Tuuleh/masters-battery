e6fe198e3d50fadc0f9173ce6239800ef9f68d5c
tuuli <tuuli.pollanen@gmail.com>
2014-09-29 13:32:20 -0700

    adding app.js with controllers and models now synced with the database and working for the finish page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var Sequelize = require('sequelize');
 var uuid = require("node-uuid");
+var crypto = require('crypto');
+var DataTypes = require("sequelize");
 
 var app = express();
 //start defining routes via app.VERB()
@@ -17,14 +19,10 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use( bodyParser.urlencoded({extended: true}) );
 //allows for serving static files
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/models'));
 
-/*
-
-
-//CHECK FROM HERE ON 
-// db config
-var env = "dev";
-var config = require('./database.json')[env];
+// gets database configurtion from database.json
+var config = require('./database.json');
 var password = config.password ? config.password : null;
 
 // initialize database connection
@@ -33,25 +31,18 @@ var sequelize = new Sequelize(
     config.user,
     config.password,
     {
-        logging: console.log,
-        define: {
-            timestamps: false
-        }
+        logging: console.log
     }
 );
 
-var crypto = require('crypto');
-var DataTypes = require("sequelize");
-
-//CHECK UNTIL HERE 
-*/
 
 //app.listen() takes same args as node's net.Server#listen():
 var server = app.listen(3000, function(){
     console.log("Listening on port %d", server.address().port);
 });
 
-
+//importing finish model
+//var Finish = sequelize.import(__dirname + "/models/finish");
 
 //controllers/routers
 app.get('/', function (req, res) {
@@ -85,43 +76,61 @@ app.post('/demographics-data', function (req, res) {
     console.log(req.body.userId);
 
     res.send('{"status":"ok"}');
+    console.log("posting demographics data, queried userId is "+req.query.userId);
+    console.log("posting demographics data, body userId is "+req.body.userId);
 });
 
 app.get('/survey_with_intro', function (req, res) {
     var userId = req.query.userId;
-    console.log("logging in survey get " + req.query.userId);
-    res.render('survey_with_intro', {userId: userId});
+    res.render('survey_with_intro', {userId:userId});
+    console.log("getting survey, queried userId is "+req.query.userId);
+    console.log("getting survey, body userId is "+req.body.userId);
 });
 
 app.post('/survey_with_intro-data', function (req, res) {
     res.send('{"status":"ok"}');
+    console.log("posting survey data, queried userId is "+req.query.userId);
+    console.log("posting survey data, body userId is "+req.body.userId);
 });
 
 app.get('/flanker', function (req, res) {
     var userId = req.query.userId;
+    console.log("getting flanker data, queried userId is "+req.query.userId);
+    console.log("getting flanker data, body userId is "+req.body.userId);
     res.render('flanker', {userId:userId});
 });
 
 app.post('/flanker-data', function (req, res) {
     res.send('{"status":"ok"}');
+    console.log("posting flanker data, queried userId is "+req.query.userId);
+    console.log("posting flanker data, body userId is "+req.body.userId);
 });
 
 app.get('/mental_rotation', function (req, res) {
     var userId = req.query.userId;
     res.render('mental_rotation', {userId:userId});
+    console.log("getting mental rotation, queried userId is "+req.query.userId);
+    console.log("getting mental rotation, body userId is "+req.body.userId);
 });
 
 app.post('/mental_rotation-data', function (req, res) {
     res.send("{'status':'ok'}");
+    console.log("queried userId is "+req.query.userId);
+    console.log("posting mental_rotation data, queried userId is "+req.query.userId);
+    console.log("posting mental_rotation data, body userId is "+req.body.userId);
 });
 
 app.get('/tol', function (req, res) {
     var userId = req.query.userId;
     res.render('tol', {userId:userId});
+    console.log("getting ToL, queried userId is "+req.query.userId);
+    console.log("getting ToL rotation, body userId is "+req.body.userId);
 });
 
 app.post('/tol-data', function (req, res) {
     res.send('{"status":"ok"}');
+    console.log("posting tol data, queried userId is "+req.query.userId);
+    console.log("posting ToL data, body userId is "+req.body.userId);
 });
 
 
@@ -131,21 +140,59 @@ app.get('/palmer-experiments', function (req, res) {
 });
 
 app.post('/palmer-experiments-data', function (req, res) {
-    console.log(JSON.stringify(req.body));
     res.send('{"status":"ok"}');
 });
 
 app.get('/finish', function (req, res) {
     var userId = req.query.userId;
     res.render('finish', {userId:userId});
+    console.log("getting finish, queried userId is "+req.query.userId);
+    console.log("getting finish, body userId is "+req.body.userId);
 });
 
+
+//POST FOR FINISH:
+
 app.post('/finish-data', function (req, res) {
-    console.log(req.body.mail);
-    console.log(req.body.message);
-    console.log(req.body.wants_results);
-    console.log(req.body.thank_you_list);
-    res.send('{"status":"ok"}');
+
+    console.log("posting finish data, queried userId is "+req.query.userId);
+    console.log("posting finish data, body userId is "+req.body.userId);
+
+    //model for the finish table in thesis_database:
+
+
+    var Finish = sequelize.define('finish', {
+        user_id: {type: Sequelize.UUID, primaryKey: true}, //CHAR(36)
+        mail: DataTypes.STRING(100), //VARCHAR(100),
+        message: DataTypes.TEXT,
+        wants_results: DataTypes.INTEGER, //TINYINT,
+        thank_you: DataTypes.INTEGER, //TINYINT
+        created_at: DataTypes.DATE,
+    },
+        {
+            timestamps: true,
+            updatedAt: false,
+            deletedAt: false,
+            freezeTableName: true,
+            createdAt: 'created_at',
+        });
+
+ //Adding instances to the finish table in my database
+    Finish
+        .build({
+            user_id: req.body.userId, 
+            mail: req.body.mail, 
+            message: req.body.message, 
+            wants_results: req.body.wants_results, 
+            thank_you: req.body.thank_you
+        })
+        .save()
+        .then(function(current_finish){
+            console.log("We did it! " + current_finish.user_id)
+        }), function(err){
+            console.log("Almost there!")
+        };
+
 });
 
 app.get('/thank_you', function (req, res) {
