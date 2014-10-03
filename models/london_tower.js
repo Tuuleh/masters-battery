b4901d89c341a4d209d381792058e2b4fab84ee5
tuuli <tuuli.pollanen@gmail.com>
2014-10-02 17:14:27 -0700

    renamed and modified london tower model

---
+++ models/london_tower.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('london_tower', {
+        id: {type: DataTypes.INTEGER, primaryKey: true},
+        user_id: DataTypes.STRING(36), //CHAR(36)
+        trial_index: DataTypes.INTEGER,
+        rt: DataTypes.INTEGER,
+        moves: DataTypes.INTEGER,
+        missed_by: DataTypes.INTEGER,
+        created_at: DataTypes.DATE,
+    },
+    {
+        timestamps: true,
+        updatedAt: false,
+        deletedAt: false,
+        freezeTableName: true,
+        createdAt: 'created_at',
+    });
+}
