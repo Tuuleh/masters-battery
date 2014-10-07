24101d2539c969784f9bda1e64d17d601ff43bc4
tuuli <tuuli.pollanen@gmail.com>
2014-10-07 11:56:08 -0700

    added validation for the demographics form

--- models/demographics.js
+++ models/demographics.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
     return sequelize.define('demographics', {
         user_id: {type: DataTypes.STRING(36), primaryKey: true}, //CHAR(36)
         birth_year: DataTypes.INTEGER(11),
-        gender: DataTypes.INTEGER(11),
+        gender: DataTypes.STRING(10),
         summoner_name: {
             type: DataTypes.STRING(1000), 
             allowNull: false,
@@ -17,21 +17,56 @@ module.exports = function(sequelize, DataTypes) {
         plays_non_team: DataTypes.INTEGER(11),
         plays_3v3: DataTypes.INTEGER(11),
         plays_5v5: DataTypes.INTEGER(11),
-        non_team_queue: DataTypes.INTEGER(11),
+        non_team_queue: DataTypes.STRING(10),
         non_team_division: DataTypes.INTEGER(11), 
         non_team_tier: DataTypes.INTEGER(11),
-        team_3v3:  DataTypes.STRING(1000),
+        team_3v3:  {
+            type: DataTypes.STRING(1000), 
+            allowNull: true, 
+            validate: {
+                length: function() {
+                    if (this.plays_3v3 != undefined && (this.team_3v3.length < 2 || this.team_3v3.length > 100)) {
+                        throw new Error("Team name length is invalid.")
+                    }
+                }
+            }
+        },
         division_3v3: DataTypes.INTEGER(11),
         tier_3v3: DataTypes.INTEGER(11),
-        team_5v5: DataTypes.STRING(1000),
+        team_5v5: {
+            type: DataTypes.STRING(1000), 
+            allowNull: true, 
+            validate: {
+                length: function() {        
+                    if (this.plays_5v5 != undefined && (this.team_5v5.length < 2 || this.team_5v5.length > 100)) {
+                        throw new Error("Team name length is invalid.")
+                    }
+                }
+            }
+        },
         division_5v5: DataTypes.INTEGER(11),
         tier_5v5: DataTypes.INTEGER(11),
         created_at: DataTypes.DATE
         },{   
             validate: {
                 ranked_games: function() {
-                    if ((this.plays_non_team === null) && (this.plays_3v3 === null) && (this.plays_5v5 === null)) {
+                    if (!this.plays_non_team && !this.plays_3v3 && !this.plays_5v5) {
                         throw new Error("Select what kinds of ranked games you play.");
+                    }
+                },
+                missing_queue: function() {
+                    if (this.plays_non_team && !this.non_team_queue) {
+                        throw new Error("Select your queue style for non-team games.");
+                    }
+                },
+                missing_3v3: function() {
+                    if (this.plays_3v3 && !this.team_3v3) {
+                        throw new Error("Enter your 3v3 team name, division and  tier!");
+                    }
+                },
+                missing_5v5: function() {
+                    if (this.plays_5v5 && !this.team_5v5) {
+                        throw new Error("Enter your 5v5 team name, division and  tier!");
                     }
                 }
             },
