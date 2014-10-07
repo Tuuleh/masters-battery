f608ce5841d3fef0001a1653f2c7856caafedb18
tuuli <tuuli.pollanen@gmail.com>
2014-10-06 19:21:32 -0700

    updated some models and modified the timers on the flanker task

--- models/demographics.js
+++ models/demographics.js
@@ -3,10 +3,20 @@ module.exports = function(sequelize, DataTypes) {
         user_id: {type: DataTypes.STRING(36), primaryKey: true}, //CHAR(36)
         birth_year: DataTypes.INTEGER(11),
         gender: DataTypes.INTEGER(11),
-        summoner_name: DataTypes.STRING(1000),
+        summoner_name: {
+            type: DataTypes.STRING(1000), 
+            allowNull: false,
+            validate: {
+                notEmpty: {msg: "Please enter your summoner name."},
+                len: {args: [2,100], msg: "Name is of invalid length."}
+             }
+        },
         region: DataTypes.INTEGER(11),
         position: DataTypes.INTEGER(11),
         role: DataTypes.INTEGER(11),
+        plays_non_team: DataTypes.INTEGER(11),
+        plays_3v3: DataTypes.INTEGER(11),
+        plays_5v5: DataTypes.INTEGER(11),
         non_team_queue: DataTypes.INTEGER(11),
         non_team_division: DataTypes.INTEGER(11), 
         non_team_tier: DataTypes.INTEGER(11),
@@ -16,13 +26,19 @@ module.exports = function(sequelize, DataTypes) {
         team_5v5: DataTypes.STRING(1000),
         division_5v5: DataTypes.INTEGER(11),
         tier_5v5: DataTypes.INTEGER(11),
-        created_at: DataTypes.DATE,
-    },
-    {
-        timestamps: true,
-        updatedAt: false,
-        deletedAt: false,
-        freezeTableName: true,
-        createdAt: 'created_at',
-    });
+        created_at: DataTypes.DATE
+        },{   
+            validate: {
+                ranked_games: function() {
+                    if ((this.plays_non_team === null) && (this.plays_3v3 === null) && (this.plays_5v5 === null)) {
+                        throw new Error("Select what kinds of ranked games you play.");
+                    }
+                }
+            },
+            timestamps: true,
+            updatedAt: false,
+            deletedAt: false,
+            freezeTableName: true,
+            createdAt: 'created_at',
+        });
 }
--- models/finish.js
+++ models/finish.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('finish', {
         user_id: {type: DataTypes.STRING(36), primaryKey: true}, //CHAR(36)
-        mail: DataTypes.STRING(100), //VARCHAR(100),
+        mail: {type: DataTypes.STRING(100), allowNull: true, validate: {isEmail: {msg: "Please enter a valid e-mail address!"}}}, //VARCHAR(100),
         message: DataTypes.TEXT,
         wants_results: DataTypes.INTEGER, //TINYINT,
         thank_you: DataTypes.INTEGER, //TINYINT
