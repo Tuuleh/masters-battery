dd84717cc1b18b08b7efdf2a18dad71ba59d1c60
tuuli <tuuli.pollanen@gmail.com>
2014-10-08 13:01:31 -0700

    modified the styling for demographics to enable posting custom error messages next to impacted fields

--- models/finish.js
+++ models/finish.js
@@ -1,13 +1,33 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes, validator) {
     return sequelize.define('finish', {
-        user_id: {type: DataTypes.STRING(36), primaryKey: true}, //CHAR(36)
-        mail: {type: DataTypes.STRING(100), allowNull: true, validate: {isEmail: {msg: "Please enter a valid e-mail address!"}}}, //VARCHAR(100),
-        message: DataTypes.TEXT,
-        wants_results: DataTypes.INTEGER, //TINYINT,
-        thank_you: DataTypes.INTEGER, //TINYINT
+        user_id: {type: DataTypes.STRING(36), primaryKey: true}, 
+        mail: {
+            type: DataTypes.STRING(100), 
+            default: null, 
+            allowNull: true, 
+            validate: {
+                isEmail: {msg: "Your e-mail address is invalid!"}
+            }
+        },
+        message: {
+            type: DataTypes.TEXT, 
+            default: null,
+            allowNull: true, 
+            validate: {
+                len: {
+                    args: [0,999], 
+                    msg: "Your message is too long. You'll have to keep it below 1000 characters."
+                }
+            }
+        },
+        wants_results: DataTypes.STRING(4), 
+        thank_you: DataTypes.STRING(4), 
         created_at: DataTypes.DATE,
     },
-    {
+    {   
+        validate: {
+
+        },
         timestamps: true,
         updatedAt: false,
         deletedAt: false,
